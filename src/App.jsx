ac0c28455694d17e9c00e4e87ec2c1b8110e013f
Suwit <suwit.saiso@gmail.com>
2025-10-19 12:41:28 +0700

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Vite with Tailwind CSS!</h1>
+    </div>
+  )
+}
+
+export default App
