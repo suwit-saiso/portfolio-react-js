5e0ba2a7f839956a886b84fbbfb8edaa05851cd9
Suwit <suwit.saiso@gmail.com>
2025-10-25 15:43:42 +0700

    Fix images path

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -13,8 +13,8 @@ const Header = () => {
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
       <div className="max-w-screen-2xl w-full mx-auto px-4 md:px-6 layout-flex-grid">
         <h1>
-          <a href="/" className="logo">
-            <img src="star-logo.svg" width={40} height={40} alt="Suwit Saiso" />
+          <a href="#home" className="logo">
+            <img src={`${import.meta.env.BASE_URL}star-logo.svg`} width={40} height={40} alt="Suwit Saiso" />
           </a>
         </h1>
         <div className="relative md:justify-self-center">
